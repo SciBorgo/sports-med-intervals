4b64bb684dff51b1cf5d2cf78cfc148e09f50981
arcaldwell49 <arcaldwell49@gmail.com>
2022-06-06 09:52:31 -0400

    Running into errors
    code not working

--- 0-find-abstracts.R
+++ 0-find-abstracts.R
@@ -27,25 +27,32 @@ journals = c('Eur J Phys Rehabil Med') # restrict to years 2008 to 2022
 journals = c('Phys Sportsmed','Clin Sports Med','Phys Med Rehabil Clin N Am','J Sports Med Phys Fitness')
 
 # Pull papers
-numbers = ids = NULL
-for (j in journals){
-  for (year in 2002:2022){
-    query = paste('"', j, '"[SO] AND ', year, '[PDAT] AND (', types.search , ')', sep='')
-    journal.search = entrez_search(db='pubmed', term=query, retmax=50000)
-    # frame of numbers, store for flow diagram
-    nframe = data.frame(journal=j, year=year, count=journal.search$count) 
-    numbers = rbind(numbers, nframe)
-    # frame of IDS
-    frame = data.frame(journal=j, year=year, pubmed=journal.search$ids)
-    ids = rbind(ids, frame)
+get_abs = function(journals){
+  numbers = ids = NULL
+  for (j in journals){
+    for (year in 2002:2022){
+      query = paste('"', j, '"[SO] AND ', year, '[PDAT] AND (', types.search , ')', sep='')
+      journal.search = entrez_search(db='pubmed', term=query, retmax=50000)
+      # frame of numbers, store for flow diagram
+      nframe = data.frame(journal=j, year=year, count=journal.search$count) 
+      numbers = rbind(numbers, nframe)
+      # frame of IDS
+      frame = data.frame(journal=j, year=year, pubmed=journal.search$ids)
+      ids = rbind(ids, frame)
+    }
   }
+  return(numbers)
 }
 
+numbers = get_abs(journals = journals)
+
+
 # Plot papers each year
-ggplot(data=numbers, aes(x=year, y=count, col=factor(journal)))+
+p_paper_year = ggplot(data=numbers, aes(x=year, y=count, col=factor(journal)))+
   geom_point()+
   geom_line()+
-  scale_color_manual('Journal', values=cbPalette)+
+  #scale_color_manual('Journal', values=cbPalette)+ # cbPalette is missing
+  scale_color_viridis_d() +
   ylab('Number of abstracts')+
   xlab('Year')+
   guides(color=guide_legend(ncol=2))+
